688d2244e07e38d76c36e5b2d6648802fdbade36
mxc <mxc2013@163.com>
2026-02-06 22:13:16 -0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mxc/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mxc/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mxc/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/mxc/catkin_ws/src:$ROS_PACKAGE_PATH"
